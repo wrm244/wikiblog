e8e22935d21a25033066caa0d00d49525290af8b
wrm244 <wrm244@outlook.com>
2023-04-24 17:25:27 +0800

    🐞 fix(pwa): 修改图标路径与优化显示

--- src/plugin/plugin-baidu-push/index.js
+++ src/plugin/plugin-baidu-push/index.js
@@ -10,12 +10,7 @@ module.exports = function (context, options) {
             (function(){
               var bp = document.createElement('script');
               var curProtocol = window.location.protocol.split(':')[0];
-              if (curProtocol === 'https') {
-                  bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-              }
-              else {
-                  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-              }
+              bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
               bp.defer = true;
               var s = document.getElementsByTagName("script")[0];
               s.parentNode.insertBefore(bp, s);
