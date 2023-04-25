69ff47a9cf43cdfc285e11cbf5dd987b11d541dc
wrm244 <wrm244@outlook.com>
2023-04-25 17:39:58 +0800

    fix(ci):把部署迁移到netlity

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -226,8 +226,7 @@ const config = {
       Date("10/16/2022 8:22:00");
       Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;
       a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);
-      runtime_span.innerHTML="<br>本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();</script><br/>
-      <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script><span id="busuanzi_container_site_pv">本站总访问量<span i ="busuanzi_value_site_pv"></span>次</span>`,
+      runtime_span.innerHTML="<br>本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();</script>`,
     },
     prism: {
       theme: require('prism-react-renderer/themes/vsLight'),
